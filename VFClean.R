fb8bd854b0c55f483be5fb1337b00f06bb3cfa3c
PEBL Lab User <pebl@imac.network.ncf.edu>
2021-03-18 11:08:21 -0400

    making precinct codes match

--- VFClean.R
+++ VFClean.R
@@ -104,9 +104,84 @@ combine_clean<- combined%>% select(V1, female, age, precID, V25:V28, white, blac
 
 #comparing precID and other precinct variables to abrprecincts and fixing issues for haversine loop
 
-pollplace2018 <- read.csv("GEOCODED_2018PollPlace", header = TRUE)
+pollplace2018 <- read.csv("GEOCODED_2018PollPlace.txt", header = TRUE)
 
+#fixing volusia coding in poll data
+pollplace2018$abr <- recode(pollplace2018$abr, "127" = "VOL")
+pollplace2018$abrprecincts <- paste0(pollplace2018$abr, pollplace2018$precinct)
 
+pollplace2018$abrprecincts <- recode(pollplace2018$abrprecincts, 
+                                     "ALA1" = "ALA01", "ALA2" = "ALA02", 
+                                     "ALA3" = "ALA03", "ALA4" = "ALA04", 
+                                     "ALA5" = "ALA05", "ALA6" = "ALA06", 
+                                     "ALA7" = "ALA07", "ALA8" = "ALA08", 
+                                     "ALA9" = "ALA09")
+
+pollplace2018$abrprecincts <- recode(pollplace2018$abrprecincts, 
+                                     "FRA1" = "FRA01", "FRA2" = "FRA02", 
+                                     "FRA3" = "FRA03", "FRA4" = "FRA04", 
+                                     "FRA5" = "FRA05", "FRA6" = "FRA06", 
+                                     "FRA7" = "FRA07", "FRA8" = "FRA08")
+
+pollplace2018$abrprecincts <- recode(pollplace2018$abrprecincts, 
+                                     "LAK1" = "LAK01", "LAK2" = "LAK02", 
+                                     "LAK3" = "LAK03", "LAK4" = "LAK04", 
+                                     "LAK5" = "LAK05", "LAK6" = "LAK06", 
+                                     "LAK7" = "LAK07", "LAK8" = "LAK08", 
+                                     "LAK9" = "LAK09")
+
+pollplace2018$abrprecincts <- recode(pollplace2018$abrprecincts, 
+                                     "LEV1" = "LEV01", "LEV2" = "LEV02", 
+                                     "LEV3" = "LEV03", "LEV4" = "LEV04", 
+                                     "LEV5" = "LEV05", "LEV6" = "LEV06", 
+                                     "LEV7" = "LEV07", "LEV8" = "LEV08", 
+                                     "LEV9" = "LEV09")
+
+pollplace2018$abrprecincts <- recode(pollplace2018$abrprecincts, 
+                                     "MRN1" = "MRN0001", "MRN2" = "MRN0002", 
+                                     "MRN3" = "MRN0003", "MRN4" = "MRN0004", 
+                                     "MRN5" = "MRN0005", "MRN6" = "MRN0006", 
+                                     "MRN7" = "MRN0007", "MRN8" = "MRN0008", 
+                                     "MRN9" = "MRN0009", "MRN10" = "MRN0010", 
+                                     "MRN11" = "MRN0011", "MRN12" = "MRN0012", 
+                                     "MRN13" = "MRN0013", "MRN14" = "MRN0014", 
+                                     "MRN15" = "MRN0015", "MRN16" = "MRN0016", 
+                                     "MRN17" = "MRN0017", "MRN18" = "MRN0018", 
+                                     "MRN19" = "MRN0019", "MRN40" = "MRN0040", 
+                                     "MRN50" = "MRN0050", "MRN60" = "MRN0060", 
+                                     "MRN70" = "MRN0070")
+
+pollplace2018$abrprecincts <- recode(pollplace2018$abrprecincts, 
+                                     "MRT1" = "MRT01", "MRT2" = "MRT02", 
+                                     "MRT3" = "MRT03", "MRT4" = "MRT04", 
+                                     "MRT5" = "MRT05", "MRT6" = "MRT06", 
+                                     "MRT7" = "MRT07", "MRT8" = "MRT08", 
+                                     "MRT9" = "MRT09")
+pollplace2018$abrprecincts <- if_else(pollplace2018$abr == "PAS", 
+                                 true = (pollplace2018$abrprecincts = 
+                                 paste0(pollplace2018$abr, 
+                                        sprintf("%03s", pollplace2018$precinct))), 
+                                 false = (pollplace2018$abrprecincts = pollplace2018$abrprecincts))
+
+pollplace2018$abrprecincts <- str_replace(pollplace2018$abrprecincts, "PUT0", "PUT")
+
+pollplace2018$abrprecincts <- if_else(pollplace2018$abr == "SEM", 
+                                      true = (pollplace2018$abrprecincts = 
+                                                paste0(pollplace2018$abr, 
+                                                       sprintf("%03s", pollplace2018$precinct))), 
+                                      false = (pollplace2018$abrprecincts = pollplace2018$abrprecincts))
+
+pollplace2018$abrprecincts <- str_replace(pollplace2018$abrprecincts, "STL0", "STL")
+pollplace2018$abrprecincts <- str_replace(pollplace2018$abrprecincts, "STL0", "STL")
+
+pollplace2018$abrprecincts <- if_else(pollplace2018$abr == "SUW", 
+                                      true = (pollplace2018$abrprecincts = 
+                                                paste0(pollplace2018$abr, 
+                                                       sprintf("%02s", pollplace2018$precinct))), 
+                                      false = (pollplace2018$abrprecincts = pollplace2018$abrprecincts))
+
+pollplace2018$abrprecincts <- str_replace(pollplace2018$abrprecincts, "WAS0", "WAS")
 
 #Write the file out as a .csv
-#write.csv(, "2018voterscleaned.csv")
+write.csv(combine_clean, "2018voterscleaned.csv")
+write.csv(pollplace2018, "adjustgeo2018polls.csv")
