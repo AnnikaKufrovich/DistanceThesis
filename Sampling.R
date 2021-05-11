363a5ef1b3042be7a8042f4df4fd2c19bc5feed9
Annika <annika@Little-Bertha.local>
2021-05-11 15:49:17 -0400

    Final comments added

--- Sampling.R
+++ Sampling.R
@@ -1,9 +1,13 @@
+library(dplyr)
+
+#similar to Predictions.R, need to have fhd2018.resonable for this to run
+
 nrow(fhd2018.reasonable)
 
-#try 3 sub samples each of various sizes to check pvals
-#then automate for whole data set if needed
+#trying 3 sub samples each of various sizes to check pvalue rankings
+#if p values bottom out can look at z scores
 
-#same seeded sample for random forest
+#same seeded sample for random forest, slicing part of it into 3 dataframes
 set.seed(795861)
 samp.rf <- fhd2018.reasonable[sample(1:nrow(fhd2018.reasonable), 2500000),] %>%
   mutate(l.inc = log(estimate))
@@ -16,6 +20,7 @@ samp2 <- samp.rf %>% slice_head(prop = .25)
 samp3 <- samp.rf %>% slice_tail(prop = 0.25)
 
 
+#binomial logistic regression fits
 binomlogit1 <- glm(data = samp1, 
                        voted2018 ~ female + age + 
                          race.eth + log(estimate) + hdistmiles + 
@@ -34,6 +39,8 @@ binomlogit3 <- glm(data = samp3,
                          voted2016b + race.eth*log(estimate), 
                        family = binomial(link = "logit"))
 
+
+#summaries, the rankings all look very similar to our standardized model rankings
 summary(binomlogit1)
 summary(binomlogit2)
 summary(binomlogit3)
